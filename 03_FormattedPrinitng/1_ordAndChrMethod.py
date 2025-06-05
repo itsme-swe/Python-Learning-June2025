51bdb05a8ce2e95719f90ea1ff972069a2754d54
harsh_sde <harsh_s.dev@outlook.com>
2025-06-05 16:41:20 +0530

    Formatted Printing

--- 03_FormattedPrinitng/1_ordAndChrMethod.py
+++ 03_FormattedPrinitng/1_ordAndChrMethod.py
@@ -1,3 +1,23 @@
-print(ord("B")) # ASCII value of 'B' is 66
+print(ord("B"))  # ASCII value of 'B' is 66
 
-print(chr(101)) # Char associated with ASCII value 101 is 'e'
+print(chr(101))  # Char associated with ASCII value 101 is 'e'
+
+print()
+
+# 💥 Printing unicodes
+s = "\u03b1"
+
+print(s + "\n")  # α -- alpha
+
+s1 = "\u03b1\u03b2\u03b3"
+
+print(s1 + "\n")  # αβγ
+
+
+name = "\u0939\u0937\u0900"
+
+print(name + "\n")  # हषऀ
+
+rupee = "\u20b9"
+
+print(rupee)  # ₹
---
+++ 03_FormattedPrinitng/2_unicodePrinting.py
@@ -0,0 +1,11 @@
+dollarSymbol = "\N{DOLLAR SIGN}"
+
+print(dollarSymbol + "\n")  # $
+
+smilely = "\N{SMILING FACE WITH SUNGLASSES}"
+
+print(smilely + "\n")  # 😎
+
+inr = "\N{RUPEE SIGN}"
+
+print(inr + "\n")
