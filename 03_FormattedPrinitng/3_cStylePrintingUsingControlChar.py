bd239ac99ee5e135910d556788577072fe781e79
harsh_sde <harsh_s.dev@outlook.com>
2025-06-05 19:35:57 +0530

    update

---
+++ 03_FormattedPrinitng/3_cStylePrintingUsingControlChar.py
@@ -0,0 +1,7 @@
+item = "Memory"
+size = 32
+price = 250.5
+
+print("Cost of %i GB %s card is ₹ %.2f" % (size, item, price))
+
+# Cost of 32 GB Memory card is ₹ 250.50
---
+++ 03_FormattedPrinitng/4_pythonFormatPrinting.py
@@ -0,0 +1,7 @@
+item = "Memory"
+size = 32
+price = 250.5
+
+print("Price of {0:^5}GB {1} card is ₹ {2}".format(size, item, price))
+
+# Price of  32  GB Memory card is ₹ 250.5
