29742f69c21061f27da4923b64318afd9b62ae03
harsh_sde <harsh_s.dev@outlook.com>
2025-06-07 18:25:44 +0530

    Dictionary data structure added

---
+++ 04_Python_DataTypes/5_Dictionary/BasicsOf Dictionary/1_creatingDictionary.py
@@ -0,0 +1,12 @@
+# 💥 Using Literal
+d = {1: "Harsh", 2: "Mukul", 3: "Harsh", 4: "James"}
+
+print(d)  # {1: 'Harsh', 2: 'Mukul', 3: 'Harsh', 4: 'James'}
+
+print()
+
+# 💥 Using dict() constructor
+
+d1 = dict(a=101, b=105, c=201, d=205)
+
+print(d1)  # {'a': 101, 'b': 105, 'c': 201, 'd': 205}
