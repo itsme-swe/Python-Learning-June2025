37bd9c51f37470599c90f9856d75d60c9f1ac441
harsh_sde <harsh_s.dev@outlook.com>
2025-06-08 12:52:18 +0530

    update args

--- 05_Functions/7_variableLengthPositionalArgs.py
+++ 05_Functions/7_variableLengthPositionalArgs.py
@@ -6,4 +6,16 @@ def myFun(*args):
 
 myFun("Hello", "I am", "Harsh", "Mehra")  # Hello I am Harsh Mehra
 
+print()
+
+
+def displayIntOnly(*args):
+    for i in args:
+        if type(i) == int:
+            print(i, end=" ")
+
+
+displayIntOnly("harsh", 33, 71.5, True, 15)  # 33 15
+
+
 # 💥  If we use *args we can pass a variable number of arguments to a function using special symbol.
