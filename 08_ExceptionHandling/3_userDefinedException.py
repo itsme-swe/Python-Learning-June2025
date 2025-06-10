a9a9b95bfa571d12b75800121e8b8e0e2aed709a
harsh_sde <harsh_s.dev@outlook.com>
2025-06-10 16:57:18 +0530

    update

--- 08_ExceptionHandling/3_userDefinedException.py
+++ 08_ExceptionHandling/3_userDefinedException.py
@@ -1,15 +1,22 @@
 class NegativeError(Exception):
-    pass
+
+    def __init__(self):
+        self.msg = "-ve dimensions not allowed!!"
+
+    def __str__(self):
+        return self.msg
 
 
 def area(len, bre):
     if len >= 0 and bre >= 0:
         return len * bre
     else:
-        raise NegativeError("-ve dimensions not allowed!!")
+        raise NegativeError()
 
 
 try:
-    print(area(2, 5))
+    print(area(-5, 5))
 except NegativeError as ne:
     print(ne)
+
+# op:  -ve dimensions not allowed!!
