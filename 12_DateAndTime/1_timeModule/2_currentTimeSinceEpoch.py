742a53e95cd9157f41dd79954ce9789dae15cbb1
harsh_sde <harsh_s.dev@outlook.com>
2025-06-16 05:48:08 +0530

    DateAndTime added

---
+++ 12_DateAndTime/1_timeModule/2_currentTimeSinceEpoch.py
@@ -0,0 +1,11 @@
+import time
+
+currTm = time.time()
+
+print(f"Current time in seconds since from epoch: {currTm}")
+
+# Current time in seconds since from epoch: 1750032365.1916652
+
+currTmInString = time.ctime()
+
+print(f"Current time is: {currTmInString}")  # Current time is: Mon Jun 16 05:36:05 2025
