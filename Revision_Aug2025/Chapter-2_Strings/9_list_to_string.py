d4388bd86994842996400a9de540957a102dab77
itsharsh_swe <harsh_s.dev@outlook.com>
2025-07-29 07:48:35 +0530

    update

---
+++ Revision_Aug2025/Chapter-2_Strings/9_list_to_string.py
@@ -0,0 +1,7 @@
+# 🔶 The join() method in Python is used to concatenate the elements of an iterable (such as a list, tuple, or set) into a single string with a specified delimiter placed between each element.
+
+need_to_visit = ["Leh", "Nubra", "Pangong", "Hanle", "Umilang"]
+
+str = ", ".join(need_to_visit)
+
+print(str)  # Leh, Nubra, Pangong, Hanle, Umilang
